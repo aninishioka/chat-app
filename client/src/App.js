3de820aedb345ae8041657272534945edfc52d6d
Anissa Nishioka <aninishioka@berkeley.edu>
2023-01-15 13:29:06 -0800

    basic socket.io setup

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,11 @@ import CurrentChat from "./Components/CurrentChat";
 import Sidebar from "./Components/Sidebar";
 import { Route, Routes } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
 
 function App() {
   const [data, setData] = useState([{}]);
+  const socket = io("http://localhost:5000");
 
   useEffect(() => {
     fetch("/chats")
@@ -15,6 +17,10 @@ function App() {
       })
       .then((data) => setData(data))
       .catch((err) => console.log(err));
+
+    socket.on("connect", () => {
+      console.log(`connected to ${socket.id}`);
+    });
   }, []);
 
   return (
--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const chats = require("./Routes/chats");
+const socketio = require("socket.io");
+const http = require("http");
+
 const app = express();
-const port = 5000;
+const server = http.createServer(app);
+const port = process.env.port || 5000;
+const io = socketio(server, {
+  cors: {
+    origin: ["http://localhost:3000"],
+  },
+});
 
 app.use("/chats", chats);
 
-app.listen(port, () => {
+io.on("connection", (socket) => {});
+
+server.listen(port, () => {
   console.log("app listening on port 5000");
 });

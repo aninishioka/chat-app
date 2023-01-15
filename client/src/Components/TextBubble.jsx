509b04dd4075237139d3564d240b6b30542d6084
Anissa Nishioka <aninishioka@berkeley.edu>
2023-01-15 14:01:55 -0800

    make text bubbles less rounded

--- client/src/Components/TextBubble.jsx
+++ client/src/Components/TextBubble.jsx
@@ -4,9 +4,7 @@ import "./CSS/TextBubble.css";
 class TextBubble extends Component {
   render() {
     return (
-      <div
-        className={"textBubble rounded-pill align-middle " + this.props.sender}
-      >
+      <div className={"textBubble rounded align-middle " + this.props.sender}>
         <span className="textBubble__message">{this.props.message}</span>
       </div>
     );
